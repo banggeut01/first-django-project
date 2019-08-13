4ed18166a65544dd7b41ef8d6dc47015d89110f7
banggeut01 <genie121110@gmail.com>
2019-08-13 13:48:13 +0900

    startapp services2 다시

--- first_django/first_django/settings.py
+++ first_django/first_django/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages',
-    'services',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
